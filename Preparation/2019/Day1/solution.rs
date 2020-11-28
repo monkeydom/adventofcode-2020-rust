71d08ed2fd3c6b8a94700f336b4cba73995143bd
Dominik Wagner <wagnr@icloud.com>
2020-11-28 15:06:33 +0100

    2019-01 part2 solved

--- Preparation/2019/Day1/solution.rs
+++ Preparation/2019/Day1/solution.rs
@@ -1,6 +1,10 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
+fn fuel_consumption(mass : i64) -> i64 {
+    std::cmp::max(mass / 3 - 2, 0)
+}
+
 fn main() -> io::Result<()> {
     println!("Hello World!");
 
@@ -8,14 +12,22 @@ fn main() -> io::Result<()> {
     let reader = BufReader::new(file);
     
     let mut totalFuel : i64 = 0;
+    let mut totalFuelPart2 : i64 = 0;
 
     for number in reader.lines().map(|l| l.unwrap().parse::<i64>()) {
         let mass = number.unwrap();
-        let fuel = mass / 3 - 2;
+        let mut fuel = fuel_consumption(mass);
         totalFuel += fuel;
-        println!("{} {} -> total fuel {}", mass, fuel, totalFuel);
-
+        print!("{} | {}", mass, fuel);
+        while fuel > 0 {
+            totalFuelPart2 += fuel;
+            fuel = fuel_consumption(fuel);
+            print!(" {}", fuel);
+        }
+        println!("");
     }
+
+    println!("Solution to Part1: {}\nSolution to Part2: {}", totalFuel, totalFuelPart2);
 
     Ok(())
 }

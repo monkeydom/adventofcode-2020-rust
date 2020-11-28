69904b35ccb63f516955b66dd0a8447a66465060
Dominik Wagner <wagnr@icloud.com>
2020-11-28 14:31:39 +0100

    now reading a file line by line

--- Preparation/2019/Day1/solution.rs
+++ Preparation/2019/Day1/solution.rs
@@ -1,3 +1,15 @@
-fn main() {
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+fn main() -> io::Result<()> {
     println!("Hello World!");
+
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }

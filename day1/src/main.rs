335b6847226915dc591d4485d2b4ecc82a0845bc
dom <wagnr@icloud.com>
2020-12-01 09:08:40 +0100

    Solved 2020 Day 1 Part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -7,17 +7,42 @@ fn main() -> io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
 
-    let input_numbers = reader.lines().map(|l| {
-        l.unwrap()
-            .parse::<i64>()
-            .expect("Expected parseable number in each line")
-    });
+    let mut input_numbers: Vec<i64> = reader
+        .lines()
+        .map(|l| {
+            l.unwrap()
+                .parse::<i64>()
+                .expect("Expected parseable number in each line")
+        })
+        .collect();
 
-    for number in input_numbers {
+    input_numbers.sort();
+
+    let mut reversed = input_numbers.clone();
+    reversed.reverse();
+
+    let threshold = 2020;
+    let mut a = -1;
+    let mut b = -1;
+
+    'outer: for number in input_numbers {
         print!("{} ", number);
+        for other in &reversed {
+            let sum = *other + number;
+            if sum < threshold {
+                println!("Sum to low {} + {} = {}", *other, number, sum);
+                break;
+            } else if sum == threshold {
+                println!("Sum good {} + {} = {}", *other, number, sum);
+                a = number;
+                b = *other;
+                break 'outer;
+            }
+        }
     }
 
     println!("");
+    println!("Solution Part 1: {} * {} = {}", a, b, a * b);
     println!("Done");
 
     Ok(())

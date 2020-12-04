735af32b4009aef121e3e25c212bc4b8f0547475
dom <wagnr@icloud.com>
2020-12-04 09:32:51 +0100

    2020 Day 4 - setup with some ansi testing

---
+++ day4/src/main.rs
@@ -0,0 +1,17 @@
+mod aoc;
+mod file;
+
+fn main() {
+    aoc::preamble();
+
+    part1();
+    part2();
+}
+
+fn part1() {
+    aoc::print_solution1("Not Yet");
+}
+
+fn part2() {
+    aoc::print_solution2("Not Yet");
+}

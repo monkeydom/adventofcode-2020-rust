1384b036d8578cd0600773e3b08ae978f25b501c
dom <wagnr@icloud.com>
2020-12-06 13:47:44 +0100

    2020 Day 6 Part 2 - Solved

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -5,7 +5,6 @@ mod file;
 // use std::{collections::HashSet, ops::RangeBounds};
 use std::collections::HashSet;
 
-
 fn main() {
     aoc::preamble();
     part1();
@@ -15,7 +14,7 @@ fn main() {
 fn part1() {
     let mut chars: HashSet<char> = HashSet::new();
 
-let mut result = 0;
+    let mut result = 0;
 
     for (i, section) in file::sections().enumerate() {
         let mut count = 0;
@@ -26,12 +25,40 @@ let mut result = 0;
             count += 1;
         }
         result += chars.len();
-        println!("{}: {} lines {:?}", i, count, chars.iter().sorted().collect::<String>());
+        println!(
+            "{}: {} lines {:?}",
+            i,
+            count,
+            chars.iter().sorted().collect::<String>()
+        );
         chars.drain();
     }
     aoc::print_solution1(format!("{} combined yes answers", result).as_str());
 }
 
 fn part2() {
-    aoc::print_solution2(format!("{}", "none yet").as_str());
+    let mut chars: HashSet<char> = HashSet::new();
+    let mut result = 0;
+
+    for (i, section) in file::sections().enumerate() {
+        let mut count = 0;
+
+        for (pos, line) in section.enumerate() {
+            if pos == 0 {
+                chars.extend(line.chars());
+            } else {
+                chars = line.chars().filter(|c| chars.contains(c)).collect();
+            }
+            count += 1;
+        }
+        result += chars.len();
+        println!(
+            "{}: {} lines {:?}",
+            i,
+            count,
+            chars.iter().sorted().collect::<String>()
+        );
+        chars.drain();
+    }
+    aoc::print_solution2(format!("{} all yes answers", result).as_str());
 }

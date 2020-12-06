d7c4c9372cc93b424edc79b0856041182d897336
dom <wagnr@icloud.com>
2020-12-06 12:58:54 +0100

    2020 Day 6 - basic setup

---
+++ day6/src/aoc.rs
@@ -0,0 +1,63 @@
+use ansi_term::Colour::{Cyan, Green};
+use ansi_term::Style;
+use ansi_term::{ANSIString, ANSIStrings};
+
+const NAME: &'static str = env!("CARGO_PKG_NAME");
+
+fn some_kind_of_uppercase_first_letter(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().chain(c).collect(),
+    }
+}
+
+pub fn preamble() {
+    let name = some_kind_of_uppercase_first_letter(&NAME[..3]);
+    let number = &NAME[3..];
+
+    let line: &[ANSIString<'static>] = &[
+        Style::new().bold().paint("2020 "),
+        Cyan.bold().paint(name),
+        Style::new().bold().paint(format!(" {}", number)),
+    ];
+
+    let ansi_line = ANSIStrings(&line);
+    println!("{}", &ansi_line);
+    let line_length = line.iter().fold(0, |acc, l| acc + l.len());
+
+    println!("{}", Style::new().bold().paint("=".repeat(line_length)));
+}
+
+fn print_solution(s: &str, part: i8) {
+    let line: &[ANSIString] = &[
+        Style::new()
+            .bold()
+            .underline()
+            .paint(format!("Solution Part {}:", part)),
+        Style::new().paint(" "),
+        Green.bold().paint(s),
+    ];
+
+    let ansi_line = ANSIStrings(line);
+    println!("{}", ansi_line);
+}
+
+pub fn print_solution1(s: &str) {
+    print_solution(s, 1);
+}
+
+pub fn print_solution2(s: &str) {
+    print_solution(s, 2);
+}
+
+pub fn print_key_value(key: &str, value: &str) {
+    let line: &[ANSIString] = &[
+        Style::new().italic().paint(format!("{}:", key)),
+        Style::new().paint(" "),
+        Style::new().bold().paint(value),
+    ];
+
+    let ansi_line = ANSIStrings(line);
+    println!("{}", ansi_line);
+}
---
+++ day6/src/main.rs
@@ -0,0 +1,18 @@
+mod aoc;
+mod file;
+use ansi_term::Colour::Red;
+use std::{collections::HashSet, ops::RangeBounds};
+
+fn main() {
+    aoc::preamble();
+    part1();
+    part2();
+}
+
+fn part1() {
+    aoc::print_solution1(format!("{}", "none yet").as_str());
+}
+
+fn part2() {
+    aoc::print_solution2(format!("{}", "none yet").as_str());
+}

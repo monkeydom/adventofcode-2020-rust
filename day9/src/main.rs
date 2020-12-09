ffc24004f959c322ec1e6c6403d3565e9df85e97
dom <wagnr@icloud.com>
2020-12-09 09:37:26 +0100

    2020 Day 9 - Setup

---
+++ day9/src/main.rs
@@ -0,0 +1,29 @@
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+
+#[allow(dead_code)]
+mod aoc;
+#[allow(dead_code)]
+mod file;
+
+// use itertools::Itertools;
+// use ansi_term::Colour::Red;
+// use std::{collections::HashSet, ops::RangeBounds};
+// use std::collections::HashMap;
+use std::collections::HashSet;
+// use std::fmt;
+
+fn main() {
+    aoc::preamble();
+    part1();
+    part2();
+}
+
+fn part1() {
+    let result = "None Yet";
+    aoc::print_solution1(format!("{:?}", result).as_str());
+}
+
+fn part2() {
+    let result = "None Yet";
+    aoc::print_solution2(format!("{:?} ", result).as_str());
+}

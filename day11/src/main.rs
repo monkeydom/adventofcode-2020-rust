cb55722f7cd066115672a2e49736241a7202f812
Dominik Wagner <wagnr@icloud.com>
2020-12-13 02:05:32 +0100

    2020 Day 11 Part 2 - Solved

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -58,11 +58,11 @@ impl Location {
 
 fn main() {
     aoc::preamble();
-    part1();
+    // part1();
     part2();
 }
 
-fn stats_at_end() -> (i64,i64) {
+fn stats_at_end() -> (i64, i64) {
     let state = parse_lines(file::lines());
     let mut count = 1;
     let mut prev_stats = seat_stats(&state);
@@ -73,23 +73,103 @@ fn stats_at_end() -> (i64,i64) {
         state = next_map(&state);
         current_stats = seat_stats(&state);
         println!("Iteration {} stats: {:?}", count, current_stats);
-        count +=1;
+        count += 1;
     }
     println!("End stats {:?}", current_stats);
     current_stats
 }
 
 fn part1() {
-//    let result = "None Yet";
+    //    let result = "None Yet";
 
     let stats = stats_at_end();
     let result = stats.0;
     aoc::print_solution1(format!("{:?}", result).as_str());
 }
 
+fn generate_seat_map(seats: &HashMap<Location, SeatState>) -> HashMap<Location, Vec<Location>> {
+    let mut max_x = 0i64;
+    let mut max_y = 0i64;
+    for (loc, _) in seats {
+        max_x = max_x.max(loc.x);
+        max_y = max_y.max(loc.y);
+    }
+
+    let mut result = HashMap::new();
+    for (loc, _) in seats {
+        let mut relevants: Vec<Location> = Vec::new();
+        for x in -1i64..=1 {
+            for y in -1i64..=1 {
+                if (x, y) != (0, 0) {
+                    // search for relevance
+                    let movement = Location { x, y };
+                    let mut test_loc = *loc;
+                    loop {
+                        test_loc = test_loc + movement;
+                        if let Some(_) = seats.get(&test_loc) {
+                            relevants.push(test_loc);
+                            break;
+                        } else if (test_loc.x < 0 || test_loc.x > max_x)
+                            || (test_loc.y < 0 || test_loc.y > max_y)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        result.insert(*loc, relevants);
+    }
+    result
+}
+
+fn next_map_p2(
+    seats: &HashMap<Location, SeatState>,
+    relevants: &HashMap<Location, Vec<Location>>,
+) -> HashMap<Location, SeatState> {
+    let mut result = HashMap::new();
+    for (loc, state) in seats {
+        let seat_count = relevants[loc].iter().fold(0, |acc, test_loc| {
+            if let Some(Occupied) = seats.get(test_loc) {
+                acc + 1
+            } else {
+                acc
+            }
+        });
+        match seat_count {
+            0 => result.insert(*loc, Occupied),
+            5..=9 => result.insert(*loc, Empty),
+            _ => result.insert(*loc, *state),
+        };
+    }
+    result
+}
+
 fn part2() {
-    let result = "None Yet";
+    // let result = "None Yet";
+
+    let stats = stats_at_end_p2();
+    let result = stats.0;
     aoc::print_solution2(format!("{:?} ", result).as_str());
+}
+
+fn stats_at_end_p2() -> (i64, i64) {
+    let state = parse_lines(file::lines());
+    let seat_map = generate_seat_map(&state);
+    let mut count = 1;
+    let mut prev_stats = seat_stats(&state);
+    let mut state = next_map_p2(&state, &seat_map);
+    let mut current_stats = seat_stats(&state);
+    while prev_stats != current_stats {
+        prev_stats = current_stats;
+        state = next_map_p2(&state, &seat_map);
+        current_stats = seat_stats(&state);
+        println!("Iteration {} stats: {:?}", count, current_stats);
+        count += 1;
+    }
+    println!("End stats {:?}", current_stats);
+    current_stats
 }
 
 fn parse_lines(lines: impl Iterator<Item = String>) -> HashMap<Location, SeatState> {
@@ -113,7 +193,7 @@ fn parse_lines(lines: impl Iterator<Item = String>) -> HashMap<Location, SeatSta
 fn seat_stats(seats: &HashMap<Location, SeatState>) -> (i64, i64) {
     let mut empty_count = 0;
     let mut occupied_count = 0;
-    for (loc, state) in seats {
+    for (_loc, state) in seats {
         match state {
             Occupied => occupied_count += 1,
             Empty => empty_count += 1,
@@ -193,10 +273,9 @@ mod tests {
             state = next_map(&state);
             current_stats = seat_stats(&state);
             println!("Iteration {} stats: {:?}", count, current_stats);
-            count +=1;
+            count += 1;
         }
         println!("End stats {:?}", current_stats);
-
 
         assert_eq!(current_stats.0, 37);
     }

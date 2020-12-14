6055598b9f98fe290f99b940c0a1421f5719b9ea
Dominik Wagner <wagnr@icloud.com>
2020-12-14 09:51:00 +0100

    2020 Day 14 Part 1 - solved

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -10,7 +10,7 @@ mod tests;
 // use itertools::Itertools;
 // use ansi_term::Colour::Red;
 // use std::{collections::HashSet, ops::RangeBounds};
-// use std::collections::HashMap;
+use std::collections::HashMap;
 // use std::collections::HashSet;
 // use std::fmt;
 
@@ -23,7 +23,9 @@ fn main() {
 }
 
 fn part1() {
-    let result = "None Yet";
+    // let result = "None Yet";
+    let mem = process_lines(file::lines());
+    let result = sum_memory(&mem);
     aoc::print_solution1(format!("{:?}", result).as_str());
 }
 
@@ -39,4 +41,51 @@ where
     lines
         .map(|l| l.parse::<i64>().expect("Needs to be a parseable number"))
         .collect()
+}
+
+fn sum_memory(mem: &HashMap<usize, u64>) -> u64 {
+    mem.iter().fold(0u64, |acc, (_, v)| acc + *v)
+}
+
+fn parse_mask(mask: &str) -> (u64, u64) {
+    let mut set_mask = 0u64;
+    let mut clear_mask = 0u64;
+
+    for (loc, c) in mask.chars().enumerate() {
+        match c {
+            '1' => set_mask |= 1u64 << (35 - loc),
+            '0' => clear_mask |= 1u64 << (35 - loc),
+            _ => (),
+        }
+    }
+
+    (set_mask, clear_mask)
+}
+
+fn process_lines(lines: impl Iterator<Item = String>) -> HashMap<usize, u64> {
+    let mut result = HashMap::new();
+
+    let mut set_mask = 0u64;
+    let mut clear_mask = 0u64;
+
+    for line in lines {
+        let parts: Vec<&str> = line.split(" = ").collect();
+        match parts[0] {
+            "mask" => {
+                let pm = parse_mask(parts[1]);
+                set_mask = pm.0;
+                clear_mask = pm.1;
+            }
+
+            _ => {
+                let loc: usize = parts[0][4..(parts[0].len() - 1)].parse().unwrap();
+                let value: u64 = parts[1].parse().unwrap();
+                let value = value | set_mask;
+                let value = value & !clear_mask;
+                result.insert(loc, value);
+            }
+        }
+    }
+
+    result
 }

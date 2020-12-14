ebcdac5e5b21ad3ac03378a3b1b528d5837c29a7
Dominik Wagner <wagnr@icloud.com>
2020-12-14 10:39:36 +0100

    passing tests

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -47,19 +47,20 @@ fn sum_memory(mem: &HashMap<usize, u64>) -> u64 {
     mem.iter().fold(0u64, |acc, (_, v)| acc + *v)
 }
 
-fn parse_mask(mask: &str) -> (u64, u64) {
+pub fn parse_mask(mask: &str) -> (u64, u64, Vec<u64>) {
     let mut set_mask = 0u64;
     let mut clear_mask = 0u64;
+    let mut fluc = vec![];
 
     for (loc, c) in mask.chars().enumerate() {
         match c {
             '1' => set_mask |= 1u64 << (35 - loc),
             '0' => clear_mask |= 1u64 << (35 - loc),
-            _ => (),
+            _ => fluc.push(1u64 << (35 - loc)),
         }
     }
 
-    (set_mask, clear_mask)
+    (set_mask, clear_mask, fluc)
 }
 
 fn process_lines(lines: impl Iterator<Item = String>) -> HashMap<usize, u64> {
@@ -83,6 +84,51 @@ fn process_lines(lines: impl Iterator<Item = String>) -> HashMap<usize, u64> {
                 let value = value | set_mask;
                 let value = value & !clear_mask;
                 result.insert(loc, value);
+            }
+        }
+    }
+
+    result
+}
+
+fn all_fluc_from_fluc(fluc: &Vec<u64>) -> Vec<u64> {
+    let mut all_fluc = vec![0u64];
+    for n in fluc {
+        for o in all_fluc.clone() {
+            all_fluc.push(*n + o);
+        }
+    }
+    all_fluc
+}
+
+fn process_lines_p2(lines: impl Iterator<Item = String>) -> HashMap<usize, u64> {
+    let mut result = HashMap::new();
+
+    let mut set_mask = 0u64;
+    let mut clear_mask = 0u64;
+    let mut all_fluc = vec![0u64];
+
+    for line in lines {
+        let parts: Vec<&str> = line.split(" = ").collect();
+        match parts[0] {
+            "mask" => {
+                let pm = parse_mask(parts[1]);
+                all_fluc = all_fluc_from_fluc(&pm.2);
+                println!("{:?} {:?}", &pm, &all_fluc);
+                set_mask = pm.0;
+                clear_mask = pm.1;
+            }
+
+            _ => {
+                let loc: usize = parts[0][4..(parts[0].len() - 1)].parse().unwrap();
+                let value: u64 = parts[1].parse().unwrap();
+
+                let base_loc = loc & clear_mask as usize;
+                let base_loc = base_loc | set_mask as usize;
+
+                for n in &all_fluc {
+                    result.insert(base_loc + *n as usize, value);
+                }
             }
         }
     }
--- day14/src/tests.rs
+++ day14/src/tests.rs
@@ -20,6 +20,14 @@ fn test_lines() -> impl Iterator<Item = String> {
     source.split("\n").map(|l| l.to_string())
 }
 
+fn test_lines2() -> impl Iterator<Item = String> {
+    let source = indoc! {"mask = 000000000000000000000000000000X1001X
+    mem[42] = 100
+    mask = 00000000000000000000000000000000X0XX
+    mem[26] = 1"};
+    source.split("\n").map(|l| l.to_string())
+}
+
 #[test]
 fn test_part1() {
     let lines: Vec<String> = test_lines().collect();
@@ -30,9 +38,16 @@ fn test_part1() {
 }
 
 #[test]
+fn test_part2() {
+    let lines: Vec<String> = test_lines2().collect();
+    println!("{:?}", lines);
+    let mem = process_lines_p2(test_lines2());
+    println!("{:?}", &mem);
+    assert_eq!(sum_memory(&mem), 208);
+}
+
+#[test]
 fn test_parse_mask() {
-    assert_eq!(
-        parse_mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X"),
-        (1 << 6, 1 << 1)
-    );
+    let (a, b, _) = parse_mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X");
+    assert_eq!((a, b), (1 << 6, 1 << 1));
 }

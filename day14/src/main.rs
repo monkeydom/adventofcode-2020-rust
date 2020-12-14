843429b12624c1f1a936e4cdb89195c2a56127b4
Dominik Wagner <wagnr@icloud.com>
2020-12-14 09:15:17 +0100

    2020 Day 14 - Setup

---
+++ day14/src/main.rs
@@ -0,0 +1,70 @@
+#![cfg_attr(debug_assertions, allow(dead_code, unused_imports))]
+
+#[allow(dead_code)]
+mod aoc;
+#[allow(dead_code)]
+mod file;
+
+// use itertools::Itertools;
+// use ansi_term::Colour::Red;
+// use std::{collections::HashSet, ops::RangeBounds};
+// use std::collections::HashMap;
+// use std::collections::HashSet;
+// use std::fmt;
+
+use std::cmp::Ordering;
+
+fn main() {
+    aoc::preamble();
+    part1();
+    part2();
+}
+
+fn part1() {
+    let result = "None Yet";
+    aoc::print_solution1(format!("{:?}", result).as_str());
+}
+
+fn part2() {
+    let result = "None Yet";
+    aoc::print_solution2(format!("{:?} ", result).as_str());
+}
+
+fn parse_numbers<T>(lines: T) -> Vec<i64>
+where
+    T: Iterator<Item = String>,
+{
+    lines
+        .map(|l| l.parse::<i64>().expect("Needs to be a parseable number"))
+        .collect()
+}
+
+// note
+// this outputs standard io while running tests
+// cargo test -- --nocapture
+
+// also cool (needs cargo-watch installed)
+// cargo watch -x "test -- --nocapture"
+
+use indoc::indoc;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    fn test_lines() -> impl Iterator<Item = String> {
+        let source = indoc! {"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+							mem[8] = 11
+							mem[7] = 101
+							mem[8] = 0"};
+        source.split("\n").map(|l| l.to_string())
+    }
+
+    #[test]
+    fn test_part1() {
+        let lines: Vec<String> = test_lines().collect();
+        println!("{:?}", lines);
+        assert_eq!(true, false);
+    }
+}
